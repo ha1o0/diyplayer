8e935d94642787e018c2f0c4411b412e68c2cf94
Sidney Li <sidneyli@augmentum.com.cn>
2019-04-01 15:38:33 +0800

    feat(network): add alamofire and swift json demo

--- diyplayer/diyplayer/HomeViewController.swift
+++ diyplayer/diyplayer/HomeViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SnapKit
+import Alamofire
+import SwiftyJSON
 
 class HomeViewController: UIViewController {
 
@@ -24,9 +26,43 @@ class HomeViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let playerView = DiyPlayerView(frame: CGRect(x: 0, y: 20, width: 375, height: 250))
+        let playerView = DiyPlayerView(frame: CGRect(x: 0, y: 40, width: UIScreen.main.bounds.width, height: 250))
         playerView.commonInit()
         self.view.addSubview(playerView)
+        let uiView = UIView()
+        uiView.backgroundColor = UIColor.gray
+        self.view.addSubview(uiView)
+        uiView.snp.makeConstraints { (maker) in
+            maker.left.equalToSuperview().offset(0)
+            maker.trailing.equalToSuperview().offset(0)
+            maker.top.equalTo(playerView.snp.bottom).offset(20)
+            maker.height.equalTo(50)
+        }
+
+        let requestBtn = UIButton()
+        requestBtn.setTitle("请求数据", for: .normal)
+        requestBtn.setTitleColor(UIColor.white, for: .normal)
+        uiView.addSubview(requestBtn)
+        requestBtn.snp.makeConstraints { (maker) in
+            maker.center.equalToSuperview()
+        }
+        requestBtn.addTarget(self, action: #selector(getNetData), for: .touchUpInside)
+    }
+
+    @objc func getNetData() {
+        let url = "https://www.sojson.com/api/beian/sojson.com"
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                let nowIcp = json["nowIcp"].stringValue
+                print(nowIcp)
+            case .failure(let error):
+                print(error)
+            }
+        }
+        print("00000")
     }
 
 }
