6564a870a5bea70144e4b95eee4c671267aea7c2
Sidney Li <sidneyli@augmentum.com.cn>
2019-04-02 15:54:45 +0800

    feat(app): add gesture to play/pause and set volume of player

--- diyplayer/diyplayer/HomeViewController.swift
+++ diyplayer/diyplayer/HomeViewController.swift
@@ -14,6 +14,8 @@ import SwiftyJSON
 class HomeViewController: UIViewController {
 
     var diyPlayerView = DiyPlayerView()
+    var responseButton = UIButton()
+    var domainName = UITextField()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -29,40 +31,79 @@ class HomeViewController: UIViewController {
         let playerView = DiyPlayerView(frame: CGRect(x: 0, y: 40, width: UIScreen.main.bounds.width, height: 250))
         playerView.commonInit()
         self.view.addSubview(playerView)
+
+        // input domain
+        let uiInputView = UIView()
+        uiInputView.backgroundColor = UIColor.gray
+        self.view.addSubview(uiInputView)
+        uiInputView.snp.makeConstraints { (maker) in
+            maker.left.equalToSuperview().offset(0)
+            maker.trailing.equalToSuperview().offset(0)
+            maker.top.equalTo(playerView.snp.bottom).offset(20)
+            maker.height.equalTo(50)
+        }
+        uiInputView.addSubview(self.domainName)
+        self.domainName.snp.makeConstraints { (maker) in
+            maker.height.equalTo(40)
+            maker.width.equalTo(350)
+            maker.center.equalToSuperview()
+        }
+        self.domainName.placeholder = "json.cn"
+        self.domainName.text = "json.cn"
+        // query button
         let uiView = UIView()
         uiView.backgroundColor = UIColor.gray
         self.view.addSubview(uiView)
         uiView.snp.makeConstraints { (maker) in
             maker.left.equalToSuperview().offset(0)
             maker.trailing.equalToSuperview().offset(0)
-            maker.top.equalTo(playerView.snp.bottom).offset(20)
+            maker.top.equalTo(uiInputView.snp.bottom).offset(20)
             maker.height.equalTo(50)
         }
-
         let requestBtn = UIButton()
-        requestBtn.setTitle("请求数据", for: .normal)
+        requestBtn.setTitle("查询域名信息", for: .normal)
         requestBtn.setTitleColor(UIColor.white, for: .normal)
+        requestBtn.setTitleColor(UIColor.blue, for: .highlighted)
         uiView.addSubview(requestBtn)
         requestBtn.snp.makeConstraints { (maker) in
             maker.center.equalToSuperview()
         }
         requestBtn.addTarget(self, action: #selector(getNetData), for: .touchUpInside)
+
+        // response
+        let uiResponseView = UIView()
+        uiResponseView.backgroundColor = UIColor.gray
+        self.view.addSubview(uiResponseView)
+        uiResponseView.snp.makeConstraints { (maker) in
+            maker.left.equalToSuperview().offset(0)
+            maker.trailing.equalToSuperview().offset(0)
+            maker.top.equalTo(uiView.snp.bottom).offset(20)
+            maker.height.equalTo(50)
+        }
+        responseButton.setTitleColor(UIColor.white, for: .normal)
+        uiResponseView.addSubview(responseButton)
+        responseButton.snp.makeConstraints { (maker) in
+            maker.center.equalToSuperview()
+        }
+
     }
 
     @objc func getNetData() {
-        let url = "https://www.sojson.com/api/beian/sojson.com"
+        let domain = self.domainName.text ?? "json.cn"
+        let url = "https://www.sojson.com/api/beian/\(domain)"
         Alamofire.request(url, method: .get).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
                 print("JSON: \(json)")
                 let nowIcp = json["nowIcp"].stringValue
-                print(nowIcp)
+                let name = json["name"].stringValue
+                let result = name == "" ? json["message"].stringValue : "\(name),\(nowIcp)"
+                self.responseButton.setTitle(result, for: .normal)
+
             case .failure(let error):
                 print(error)
             }
         }
-        print("00000")
     }
-
 }
