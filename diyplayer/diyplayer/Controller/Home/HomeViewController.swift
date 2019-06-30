ef059e5fc08a3e82eb332dd3933e56b273bae538
weifengsmile <516202795@qq.com>
2019-07-01 00:26:42 +0800

    feat(iOS): add home page and remove pop gesture of home page

--- diyplayer/diyplayer/Controller/Home/HomeViewController.swift
+++ diyplayer/diyplayer/Controller/Home/HomeViewController.swift
@@ -7,16 +7,38 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let jumpBtn = UIButton()
+        jumpBtn.setTitle("进入视频播放页", for: .normal)
+        jumpBtn.setTitleColor(UIColor.black, for: .normal)
+        jumpBtn.setTitleColor(UIColor.blue, for: .highlighted)
+        jumpBtn.addTarget(self, action: #selector(toPlayView), for: .touchUpInside)
+        self.view.addSubview(jumpBtn)
+        jumpBtn.snp.makeConstraints { (maker) in
+            maker.centerX.equalTo(self.view)
+            maker.centerY.equalTo(self.view)
+        }
+        
         // Do any additional setup after loading the view.
     }
 
+    @objc func toPlayView() {
+        self.navigationController?.pushViewController(PlayViewController(), animated: true)
+    }
 
+    override func viewWillDisappear(_ animated:Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+    }
+    override func viewDidAppear(_ animated:Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
     /*
     // MARK: - Navigation
 
