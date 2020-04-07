4db75080c3f6a5a5519259a18fb0eeceff8be477
sidney <516202795@qq.com>
2020-04-08 00:00:44 +0800

    feat(iOS): add collectionView on livelistview

--- diyplayer/diyplayer/View/LiveListView.swift
+++ diyplayer/diyplayer/View/LiveListView.swift
@@ -7,16 +7,71 @@
 //
 
 import UIKit
+import SnapKit
 
-class LiveListView: UIView {
-
+class LiveListView: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    private var collectionView: UICollectionView?
+    private var pageViewList: [UIView]?
+    private var callback : ((_ offset: CGFloat) -> ())?
+    private var currentIndex: Int?
+    
     @IBOutlet weak var testButton: UIButton!
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    
+    // MARK: dataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let count  = self.pageViewList?.count {
+            return count
+        } else {
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(indexPath.row)", for: indexPath)
+        let scrollView = UIScrollView()
+        scrollView.snp.makeConstraints { (maker) in
+            maker.top.equalToSuperview()
+            maker.bottom.equalToSuperview()
+            maker.leading.equalToSuperview()
+            maker.trailing.equalToSuperview()
+        }
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.bounces = true
+        if let view = self.pageViewList?[indexPath.row] {
+            scrollView.addSubview(view)
+            scrollView.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height)
+        }
+        cell.contentView.addSubview(scrollView)
+        return cell
+    }
+    
+    // MARK: delegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if let contentOffset = collectionView?.contentOffset {
+            if contentOffset.x == 0 {
+                callback?(0)
+            } else {
+                callback?(contentOffset.x / self.frame.width)
+            }
+        }
+    }
+    
+    func setPage(index: Int) {
+        let point = CGPoint(x: CGFloat(index) * collectionView!.frame.size.width, y: collectionView!.frame.origin.y)
+        collectionView?.setContentOffset(point, animated: true)
+        currentIndex = index
+    }
 
 }
