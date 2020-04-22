8a6c1bffc5eb5143774ef5280dd80cdee62e35c2
sidney <516202795@qq.com>
2020-04-22 22:41:58 +0800

    feat: add FreeSteam simple use

--- diyplayer/diyplayer/Controller/Channel/ChannelViewController.swift
+++ diyplayer/diyplayer/Controller/Channel/ChannelViewController.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
+import FreeStreamer
 
 class ChannelViewController: CustomViewController {
 
+    var musicUrl = "http://www.0dutv.com/plug/down/up2.php/105030812.mp3"
+    var audioStream: FSAudioStream?
+    var isInit: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        audioStream = FSAudioStream.init(url: URL(string: musicUrl))
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func play(_ sender: UIButton) {
+        if (audioStream?.isPlaying())! {
+            return
+        }
+        if isInit {
+            audioStream!.play()
+            isInit = false
+        } else {
+            audioStream?.pause()
+        }
+        
+    }
+
+    @IBAction func pause(_ sender: UIButton) {
+        if !(audioStream?.isPlaying())! {
+            return
+        }
+        audioStream!.pause()
+    }
     /*
     // MARK: - Navigation
 
