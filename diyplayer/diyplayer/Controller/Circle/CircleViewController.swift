651cd995cf374f953febce3fe4b79b06429cac22
sidney <516202795@qq.com>
2020-04-24 22:48:51 +0800

    feat: add sd-webimage

--- diyplayer/diyplayer/Controller/Circle/CircleViewController.swift
+++ diyplayer/diyplayer/Controller/Circle/CircleViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import SDWebImage
 
 class CircleViewController: CustomViewController {
 
+    @IBOutlet weak var bkgImageView: UIImageView!
+    var currentImageIndex: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +20,24 @@ class CircleViewController: CustomViewController {
     }
 
 
+    @IBAction func changeImage(_ sender: UIButton) {
+        let imageUrls = [
+            "https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/01/9b/c1/019bc110-b53d-9da2-d987-945f5fafcf95/source/600x600bb.jpg",
+            "https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/8d/17/8f/8d178ff3-cef4-b633-eeb6-55fbb2f75d35/source/600x600bb.jpg",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/ce/f5/d0/cef5d0a0-3e26-29b4-20d2-b73cd636604c/source/600x600bb.jpg"
+        ]
+        bkgImageView?.sd_setImage(with: URL(string: imageUrls[currentImageIndex]), placeholderImage: bkgImageView.image, options: SDWebImageOptions()) { (image, error, type, url) in
+            let transition = CATransition()
+            transition.duration = 0.5
+            transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+            self.bkgImageView.layer.add(transition, forKey: "a")
+            self.bkgImageView.image = image
+            self.currentImageIndex += 1
+            if self.currentImageIndex == imageUrls.count {
+                self.currentImageIndex = 0
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
